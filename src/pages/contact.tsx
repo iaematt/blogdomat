f1a19ac76ad2251ebde78f38927946ee6e313fdd
Matheus Bastos <matheusbastos@outlook.com>
2020-10-07 18:13:42 -0300

    change contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,16 +1,30 @@
 import React from 'react'
 import Head from 'next/head'
 
-import { Footer, Header } from '../../components'
+import { Footer, Header, PageTitle } from '../../components'
 
 const Contact: React.FC = () => {
   return (
     <>
       <Head>
-        <title>Contato | Blog do Mat</title>
+        <title>Contato | blogdomat</title>
       </Head>
 
       <Header />
+
+      <PageTitle title="Entre em contato">
+        Fale comigo pelo e-mail:{' '}
+        <a href="mailto:matheusbastos@outlook.com">matheusbastos@outlook.com</a>{' '}
+        <br />
+        ou pelo instagram:{' '}
+        <a
+          href="https://github.com/iaematt/blogdomat"
+          target="_blank"
+          rel="noreferrer"
+        >
+          @iaematt_
+        </a>
+      </PageTitle>
 
       <Footer />
     </>
