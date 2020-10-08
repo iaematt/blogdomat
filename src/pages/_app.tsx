95615f49b3d0876fde9a3ebdad4c9eff96fa430e
Matheus Bastos <matheusbastos@outlook.com>
2020-10-08 01:26:36 -0300

    progress page change

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,22 @@ import React from 'react'
 import Head from 'next/head'
 import { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
+import Router from 'next/router'
+import NProgress from 'nprogress'
 
 import GlobalStyle from '../styles/global'
 import theme from '../styles/theme'
+
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <>
       <Head>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="stylesheet" type="text/css" href="/nprogress.css" />
       </Head>
 
       <ThemeProvider theme={theme}>
